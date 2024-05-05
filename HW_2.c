cccb94e38ca2e028480dc439ff8cca43492dc5ff
E1B49 <u11202249@o365.ttu.edu.tw>
2024-05-06 03:14:31 +0800

    宣告函數

--- HW_2.c
+++ HW_2.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <conio.h>  // 引入標頭檔以使用 getch() 函數，實現按鍵即時響應
 
+// 函數宣告
+void showTriangle(char c);
+void printMultiplicationTable(int n);
+int handleExitOption();
+void handleTriangleOption();
+void handleMultiplicationTableOption();
+
 int main()
 {
     return 0;
