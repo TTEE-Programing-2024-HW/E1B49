7b84dcdaaa6fb97738295410ab2ef8f96f9dab14
E1B49 <u11202249@o365.ttu.edu.tw>
2024-05-05 23:56:59 +0800

    test

---
+++ hw.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>  // 引入標頭檔以使用 getch() 函數，實現按鍵即時響應
+
+// 函數宣告
+void showTriangle(char c);
+void printMultiplicationTable(int n);
+int handleExitOption();
+void handleTriangleOption();
+void handleMultiplicationTableOption();
