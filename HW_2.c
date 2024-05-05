2ed136243ef89b40b914934a6161c3d8baa6d434
E1B49 <u11202249@o365.ttu.edu.tw>
2024-05-06 03:16:21 +0800

    宣告變數

--- HW_2.c
+++ HW_2.c
@@ -11,5 +11,11 @@ void handleMultiplicationTableOption();
 
 int main()
 {
+    int password = 2024; // 預設的密碼
+    int input;           // 用戶輸入的密碼
+    int attempt = 0;     // 嘗試輸入密碼的次數
+    int choice;
+    const int max_attempts = 3; // 最大嘗試次數
+    
     return 0;
 }
