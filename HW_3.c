f4e7bcddbbcc82d1325fa753872272d4c39989c7
E1B49 <u11202249@o365.ttu.edu.tw>
2024-05-11 12:34:11 +0800

    新增個人風格畫面、ctype函式庫

--- HW_3.c
+++ HW_3.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
+#include <ctype.h>
+
+// Function to print the welcome screen
+void print_welcome_screen()
+{
+    printf("****************************************\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*         Welcome to My System         *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("****************************************\n");
+}
 
 int main()
 {
