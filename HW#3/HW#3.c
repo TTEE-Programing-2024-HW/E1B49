1a82e44baf514fc770860f938cbab5d341fc9c25
E1B49 <u11202249@o365.ttu.edu.tw>
2024-05-13 03:06:34 +0800

    實現A功能

--- HW#3/HW#3.c
+++ HW#3/HW#3.c
@@ -22,20 +22,39 @@ int findFourSeats(int seats[ROWS][COLS]);
 
 int main()
 {
+    int seats[ROWS][COLS];
+    char choice;
     int keepRunning = 1;
 
     if (checkPassword())
     {
+        initializeSeats(seats);
         do
         {
             showMainMenu();
+            fflush(stdin);
+            scanf(" %c", &choice);
+            choice = tolower(choice);
+
+            switch (choice)
+            {
+                case 'a':
+                    showSeats(seats);
+                    pressAnyKeyToContinue();
+                    system("cls"); // Clear screen and return to main menu
+                    break;
+
+                default:
+                    printf("\nInvalid input, please re-enter.\n");
+                    pressAnyKeyToContinue();
+                    break;
+            }
         } while (keepRunning);
     }
     else
     {
         printf("\nPassword input error three times, the program will end soon.");
     }
-    
 
     return 0;
 }
@@ -94,4 +113,50 @@ void showMainMenu()
     printf("|                  d. Exit                 |\n");
     printf(" ------------------------------------------\n");
     printf("Please choose one of the options¡G");
+}
+
+void pressAnyKeyToContinue()
+{
+    printf("\nPress any key to continue...");
+    getch();
+}
+
+void initializeSeats(int seats[ROWS][COLS])
+{
+    srand(time(NULL)); // Initialize random seed
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            seats[i][j] = 0; // Initialize all seats as empty
+        }
+    }
+
+    int booked = 0;
+    while (booked < 10)
+    { // Pre-book some seats randomly
+        int r = rand() % ROWS;
+        int c = rand() % COLS;
+        if (seats[r][c] == 0)
+        {
+            seats[r][c] = 1; // Mark seat as booked
+            booked++;
+        }
+    }
+}
+
+void showSeats(int seats[ROWS][COLS])
+{
+    printf("\nSeat map¡G\n");
+    printf("\n");
+    printf("  123456789\n");
+    for (int i = 0; i < ROWS; i++)
+    {
+        printf("%d ", i + 1);
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("%c", seats[i][j] == 1 ? '*' : (seats[i][j] == 2 ? '@' : '-')); // Display seat status
+        }
+        printf("\n");
+    }
 }
