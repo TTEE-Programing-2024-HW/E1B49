4a93ab0efaa21c0dc64af8e50f7af733685dc14a
E1B49 <u11202249@o365.ttu.edu.tw>
2024-05-13 02:49:20 +0800

    新增個人風格畫面、密碼輸入功能

--- HW#3/HW#3.c
+++ HW#3/HW#3.c
@@ -19,3 +19,54 @@ void chooseSeats(int seats[ROWS][COLS]);
 void acceptSuggestedSeats(int seats[ROWS][COLS]);
 void revertSuggestedSeats(int seats[ROWS][COLS]);
 int findFourSeats(int seats[ROWS][COLS]);
+
+int main()
+{
+    if (checkPassword())
+
+    return 0;
+}
+
+int checkPassword()
+{
+    int password, attempts = 0, correctPassword = 2024;
+
+    printf("****************************************\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*         Welcome to My System         *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("*                                      *\n");
+    printf("****************************************\n");
+
+    while (attempts < 3)
+    {
+        printf("Please enter password¡G");
+        fflush(stdin);
+        scanf("%d", &password);
+        if (password == correctPassword)
+        {
+            return 1;
+        }
+        else
+        {
+            printf("\nPassword incorrect¡IYou have %d attempts left.\n", 2 - attempts);
+            attempts++;
+        }
+    }
+    return 0;
+}
