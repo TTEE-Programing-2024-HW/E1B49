b13691762796b2092e8a91ecce208806046c9219
E1B49 <u11202249@o365.ttu.edu.tw>
2024-05-25 21:48:50 +0800

    新增標頭檔和定義常量

---
+++ HW#4/HW#4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
+
+#define MAX_STUDENTS 10
+#define MIN_STUDENTS 5
+#define NAME_LEN 50
+#define ID_LEN 6
+#define PASSWORD "2024"
+#define MAX_ATTEMPTS 3
